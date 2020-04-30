a484abfcc174c1c64919ed82afb5abcfa5cf7e71
siriwans <35212476+siriwans@users.noreply.github.com>
2020-04-30 00:24:45 -0700

    attempt 1

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ restService.post("/repos", function (req, res) {
   const getRepos = async () => {
     try {
       testing = "IN GETREPOS"
-      var repos = await axios.get(`https://api.github.com/users/siriwans/repos`);
+      var repos = await axios.get('https://api.github.com/users/siriwans/repos');
       testing = req.body.queryResult.parameters.userName;
       speech = repos.data.json;
       if (repos.data.statusText === 'OK') {
